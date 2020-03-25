827d837ffdc95caf36cdf3aaffbd48b2f2a6d2e5
Rishabh Kumar <61164064+rishabhkumar16@users.noreply.github.com>
2020-03-26 02:25:04 +0530

    My new portfolio website

---
+++ contact-form-handler.php
@@ -0,0 +1,23 @@
+<?php
+if(isset($_POST['submit'])){
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['messsage'];
+$subject = $_POST['subject'];
+
+$email_from = 'PORTFOLIO 2.0';
+
+$email_body = "You have received an e-main from \n\n"
+                "User Name: $name .\n\n".
+                "User Email: $visitor_email .\n\n".
+                "User Subject: $subject .\n\n".
+                "User Message: $message .\n";
+
+$to= "rishabhkumar.jsrair@gmail.com";
+$headers = "From: $email_from \r\n";
+$headers ="Reply-To: $visitor_email \r\n";
+
+mail($to,$email_body,$headers);
+header("Location: index.html?mailsent")
+}
+?>
