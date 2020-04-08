e7800ba3238fa2e94e52d7095aa5e5f4ee9020e8
Rishabh Kumar <61164064+rishabhkumar16@users.noreply.github.com>
2020-04-09 02:36:56 +0530

    Add files via upload

---
+++ animation.js
@@ -0,0 +1,20 @@
+const texts=['Java Developer','C Developer','Web Developer'];
+let count=0;
+let index=0;
+let currenttext = '';
+let letter = '';
+(function type(){
+
+    if(count === texts.length){
+        count=0;
+    }
+    currenttext=texts[count];
+    letter=currenttext.slice(0,++index);
+
+    document.querySelector('.typing').textContent=letter;
+    if(letter.length === currenttext.length){
+        count++;
+        index=0;
+    }
+    setTimeout(type,200);
+}())
